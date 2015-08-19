b231c69718a834c6bae1f5aa29d3c258a178ddbb
dkochar <deepak.kochar@amadeus.com>
2015-08-19 16:16:56 -0400

    Adding first program

---
+++ Practice/src/com/practice/java/ocjp/FirstProgram.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package com.practice.java.ocjp;
+
+/**
+ * @author dkochar
+ *
+ */
+public class FirstProgram {
+
+  public static void main(String args[]){
+    System.out.println("Change");
+  }
+
+
+}
