69b0a2b0501c027e612e728549ee5163f7326b70
dkochar <deepak2500@gmail.com>
2015-08-20 20:48:16 -0400

    Static imports

--- Practice/src/com/practice/java/ocjp/FirstProgram.java
+++ Practice/src/com/practice/java/ocjp/FirstProgram.java
@@ -3,14 +3,16 @@
  */
 package com.practice.java.ocjp;
 
+
+import static java.lang.System.out;
 /**
  * @author dkochar
  *
  */
 public class FirstProgram {
 
-  public static void main(String args[]){
-    System.out.println("Change");
+  public static void main(String x[]){
+	  out.println("Change");
   }
 
 
