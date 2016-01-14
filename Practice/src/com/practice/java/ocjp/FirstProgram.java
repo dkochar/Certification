f998e2bcd4e798e065481965f3685d9f0ccd168b
dkochar <deepak2500@gmail.com>
2016-01-13 22:53:15 -0500

    Test Issue2

--- Practice/src/com/practice/java/ocjp/FirstProgram.java
+++ Practice/src/com/practice/java/ocjp/FirstProgram.java
@@ -17,6 +17,7 @@ public class FirstProgram {
 	  out.println("Change");
 	  out.println(MAX_VALUE);
 	  out.println("This is the fix for issue1");
+	  out.println("This is the fix for test issue2");
   }
 
 
